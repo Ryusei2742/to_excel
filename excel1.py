b0216463afe01dde1953ba1e13eae3423df8fe76
Ryusei2742 <54470490+Ryusei2742@users.noreply.github.com>
2020-11-17 23:29:15 +0900

    Update excel1.py

--- excel1.py
+++ excel1.py
@@ -4,8 +4,8 @@ import openpyxl
 import pandas as pd
 import glob
 
-export_file_path = '/Users/asd2f/Python/to_excel'
-import_folder_path = '/Users/asd2f/Python/to_excel'
+export_file_path = '' #出力先
+import_folder_path = '' #読み込み元
 path = import_folder_path + '/' + '*.xlsx'
 file_path = glob.glob(path)
 # print(file_path)
@@ -22,35 +22,9 @@ df_drop = df_concat.drop('Unnamed: 0', axis = 1)
 df_sort = df_drop.sort_values(by = '数量', ascending = False)
 # print(df_sort)
 
-# df_sort.to_excel(export_file_path + '/' + 'sample_kino4.xlsx')
+# df_sort.to_excel(export_file_path + '/' + 'sample_te.xlsx')
 
-workbook = openpyxl.load_workbook(export_file_path + '/' + 'sample_kino4.xlsx')
+workbook = openpyxl.load_workbook(export_file_path + '/' + 'sample_tes.xlsx')
 worksheet = workbook.worksheets[0]
 worksheet.delete_cols(1)
-workbook.save(export_file_path + '/' + 'sample_kino4_1.xlsx')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+workbook.save(export_file_path + '/' + 'sample.xlsx')
